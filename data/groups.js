da8483ec222f0bd026553d2d36224968b906e350
cgg100-1 <chloealice.guest@gmail.com>
2026-08-15 20:26:02 +0100

    Define reusable Vinyasa group

---
+++ data/groups.js
@@ -0,0 +1,10 @@
+window.YTT_GROUPS.push({
+  id: "vinyasa",
+  title: "Vinyasa",
+  nodes: [
+    { ref: "chaturanga" },
+    { ref: "upward-facing-dog" },
+    { ref: "up-dog-to-down-dog" },
+    { ref: "downward-facing-dog" }
+  ]
+});
